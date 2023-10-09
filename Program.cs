b9a7e08db514c35b5db4874acdd297c6b6a10163
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-09 16:53:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17__The_for_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the while loop for the reference");
+            int i = 0;
+            while (i <=5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.WriteLine("the for loop");
+            for (i = 0; i  <=5; i++)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+        }
+    }
+}
